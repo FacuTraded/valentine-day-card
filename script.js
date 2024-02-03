d5b1beed5ca1e8a7f547a84ebb579d8614bb938d
FacuTraded <fd@traded.co>
2024-02-02 23:58:25 -0300

    added images

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-$(document).ready(function () {
-  $(".container")
-    .mouseenter(function () {
-      $(".card").stop().animate(
-        {
-          top: "-90px",
-        },
-        "slow"
-      );
-    })
-    .mouseleave(function () {
-      $(".card").stop().animate(
-        {
-          top: 0,
-        },
-        "slow"
-      );
-    });
+const card = document.querySelector('.card');
+const card2 = document.getElementById('card');
+const container = document.querySelector('.container');
+const buttonContainer = document.querySelector('.buttonContainer');
+const yesButton = document.querySelector('.yesButton');
+const noButton = document.querySelector('.noButton');
+
+container.addEventListener('mouseenter', () => {
+  card.style.top = '-90px';
+  buttonContainer.style.opacity = '1';
+});
+
+let catSadArray = ['cat-sad', 'cat-sad2', 'cat-sad3', 'cat-sad4', 'cat-sad5', 'cat-sad6', 'cat-sad7', 'cat-sad8', 'cat-sad9', 'cat-sad10', 'cat-sad11', 'cat-sad12', 'cat-sad13', 'cat-sad14', 'cat-sad15', 'cat-sad16', 'cat-sad17', 'cat-sad18', 'cat-sad19', 'cat-sad20']
+let catLoveArray = ['love1', 'love2', 'love3', 'love4', 'love5', 'love6', 'love7', 'love8', 'love9', 'love10', 'love11', 'love12', 'love13', 'love14', 'love15', 'love16', 'love17', 'love18', 'love19', 'love20']
+let number = 0;
+
+noButton.addEventListener('click', () => {
+  card.style.top = '-160px';
+  card.style.height = '280px';
+  card.innerHTML = `<img src="images/sad/${catSadArray[number]}.jpg" alt="gato con un arma" style="width: 100%;z-index: 1; position: relative;">`;
+  number++;
+  if (number === catSadArray.length) {
+    number = 0;
+  }
+});
+
+yesButton.addEventListener('click', () => {
+  card.style.top = '-160px';
+  card.style.height = '280px';
+  card.innerHTML = `<img src="images/love/${catLoveArray[number]}.jpg" alt="gato con un arma" style="width: 100%;z-index: 1; position: relative;">`;
+  number++;
+  if (number === catSadArray.length) {
+    number = 0;
+  }
 });
